3bba59383f5490f8686d28569b3721f35c8107c8
Mark Payne <mjpayne8@gmail.com>
2019-10-27 15:41:43 +0000

    CurrentInfo component added

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,19 @@
       <p>The aim is to get from your starting point to your end point passing through neighbouring countries</p>
       <button type="button" @click='initialiseCountries()'>Start</button>
     </div>
-    <p>{{endCountry}}</p>
+    <div v-if='Object.keys(currentCountry).length'>
+      <current-info :info = "currentCountry" ></current-info>
+    <div>
+    <div v-if='Object.keys(currentCountry).length'>
+        <p>Where to next?</p>
+        <next-country :country-list = "findCountries(currentCountry.borders)"></next-country>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurrentInfo from './components/CurrentInfo.vue'
+import NextCountry from './components/NextCountry.vue'
 
 export default {
   name: 'app',
@@ -27,12 +34,13 @@ export default {
     .then(data => this.allCountries = data);
   },
   components: {
-
+    "current-info": CurrentInfo,
+    "next-country": NextCountry
   },
   computed: {
     stepsNumber: function(){
       if(this.allCountries.includes(this.endCountry)){
-      this.getSteps(this.currentCountry,this.endCountry)
+      return this.getSteps(this.currentCountry,this.endCountry)
       }
     }
   },
@@ -49,45 +57,44 @@ export default {
     getSteps: function(countryA,countryB){
       let hasFoundCountry = false;
       let steps = 0;
-      const countriesCanTravel = [];
-      const travelCountries = [countryA]
+      let travelCountries = [countryA];
+      let countriesCanTravel = [];
       while (!hasFoundCountry){
         steps += 1;
-        // console.log(steps);
-        travelCountries.forEach(country => countriesCanTravel.concat(country.alpha3code));
-        hasFoundCountry = travelCountries.includes(countryB.alpha3Code);
+        travelCountries.forEach(country => countriesCanTravel = countriesCanTravel.concat(country.borders))
+        hasFoundCountry = countriesCanTravel.includes(countryB.alpha3Code);
+        travelCountries = this.findCountries(countriesCanTravel);
         }
       return steps;
     },
-     initialiseCountries: function(){
-      const indexOfStart = Math.floor(this.allCountries.length*Math.random());
-      this.currentCountry = this.allCountries[indexOfStart];
-      let stepCountry = [this.currentCountry];
-      let possibleCountries =[];
-      let steps = 0;
-      while(steps<3){
-        steps += 1;
-        // console.log(steps);
-        // console.log(stepCountry);
-       if (!stepCountry[0]){
-          // console.log('in if1');
-          const indexOfStart = Math.floor(this.allCountries.length*Math.random());
-          this.currentCountry = this.allCountries[indexOfStart];
-          stepCountry = [this.currentCountry];
-          steps = 0;
-          }
-        const randomNumber =  Math.floor(stepCountry[0].borders.length*Math.random());
-        console.log(randomNumber);
-        stepCountry = this.findCountries([stepCountry[0].borders[randomNumber]]);
-        }
-        this.endCountry = stepCountry[0];
-      }
-    // initialiseCountries: function(){
-    //     const chosenCountries = this.findCountries(['FRA','POL'])
-    //     console.log(chosenCountries);
-    //     this.currentCountry = chosenCountries[0];
-    //     this.endCountry = chosenCountries[1]
-    //}
+     // initialiseCountries: function(){
+     //  const indexOfStart = Math.floor(this.allCountries.length*Math.random());
+     //  this.currentCountry = this.allCountries[indexOfStart];
+     //  let stepCountry = [this.currentCountry];
+     //  let possibleCountries =[];
+     //  let steps = 0;
+     //  while(steps<3){
+     //    steps += 1;
+     //    // console.log(steps);
+     //    // console.log(stepCountry);
+     //   if (!stepCountry[0]){
+     //      // console.log('in if1');
+     //      const indexOfStart = Math.floor(this.allCountries.length*Math.random());
+     //      this.currentCountry = this.allCountries[indexOfStart];
+     //      stepCountry = [this.currentCountry];
+     //      steps = 0;
+     //      }
+     //    const randomNumber =  Math.floor(stepCountry[0].borders.length*Math.random());
+     //    console.log(randomNumber);
+     //    stepCountry = this.findCountries([stepCountry[0].borders[randomNumber]]);
+     //    }
+     //    this.endCountry = stepCountry[0];
+     //  }
+    initialiseCountries: function(){
+        const chosenCountries = this.findCountries(['FRA','POL'])
+        this.currentCountry = chosenCountries[0];
+        this.endCountry = chosenCountries[1]
+    }
   }
 }
 </script>
---
+++ src/components/CurrentInfo.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+<div>
+  <img :src="info.flag" alt="flag">
+  <ul>
+    <li>Country: {{info.name}}</li>
+    <li>Population: {{info.population}}</li>
+    <li>Region: {{info.region}}</li>
+  </ul>
+</div>
+
+
+</template>
+
+<script>
+
+
+export default {
+  props: ["info"]
+}
+</script>
+
+<style lang="css" scoped>
+  div {
+    display: flex;
+    flex: center;
+  }
+  div img{
+    height: 80px;
+    width: 160px;
+  }
+</style>
